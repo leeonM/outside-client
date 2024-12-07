fd3239966c7eac542538ad66e8e52a1311a87b1a
leeonM <110701138+leeonM@users.noreply.github.com>
2024-12-07 22:24:03 +0000

    second commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
 import AnimatedIntro from "@/components/AnimatedIntro";
 import BottomLoginSheet from "@/components/BottomLoginSheet";
+import { SplashScreen } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  SplashScreen.preventAutoHideAsync();
+  setTimeout(SplashScreen.hideAsync, 5000);
   return (
     <View
       style={{
